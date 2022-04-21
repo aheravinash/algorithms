ef841dab9d7d1bd906f2a74d3d10324254804948
Avinash Aher <aher.avinash80@gmail.com>
2022-04-21 16:06:07 +0530

    add SQRT.java

---
+++ SQRT.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class SQRT{
+	// find square root of an integer using Newton Raphson method
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		s.close();
+		System.out.println(getSQRT(n));
+	}
+	public static double getSQRT(int N){
+		double n = N,
+			   x0 = n / 2.0,
+			   x1 = 0.5 * ( x0 + n / x0),
+			   error = x1 - x0,
+			   tol = 1.0e-5;
+		if(error < 0) error = x0 - x1;
+		while(error > tol){
+			x0 = x1;
+			x1 = 0.5 * (x0 + n / x0);
+			error = x1 - x0;
+			if(error < 0) error = x0 - x1;
+		}
+		return x1;
+	}
+}
