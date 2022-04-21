f04436923c6b0811c6e4bcbd03240f9b1e25fc0d
Avinash Aher <aher.avinash80@gmail.com>
2022-04-21 16:51:24 +0530

    add Primes.java

---
+++ Primes.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.*;
+public class Primes{
+	/* a dedicated class for Prime numbers */
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+	}
+}
